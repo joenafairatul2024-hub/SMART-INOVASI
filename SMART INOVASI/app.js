0f1fdda12bf2c5c905bc422882d071e9ef09176f
joenafairatul2024-hub <joenafairatul.2024@student.uny.ac.id>
2026-08-11 08:19:29 +0700

    Update app.js

--- SMART INOVASI/app.js
+++ SMART INOVASI/app.js
@@ -620,7 +620,7 @@ async function fetchAIPrediction() {
     while (signalData.length < 100) signalData.push(Math.random());
 
     try {
-        const response = await fetch('http://127.0.0.1:8000/predict', {
+        const response = await fetch('https://pope-clubbing-unbeaten.ngrok-free.dev/predict', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ 
